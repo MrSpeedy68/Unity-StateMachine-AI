6e86042c2a45b41b2b9ead38d6d707c8898a28d0
MrSpeedy68 <47033681+MrSpeedy68@users.noreply.github.com>
2022-02-23 11:26:32 +0000

    AI 1

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,43 +6,72 @@ using UnityEngine.AI;
 
 public class NPCController : MonoBehaviour
 {
-    public List<GameObject> WayPoints;
+    public List<GameObject> wayPoints;
+    public float hearingDistance;
 
     private Animator _animator;
     private bool _isPatroling = true;
-    AnimatorStateInfo animationStateInfo;
-    private int WPCount = 0;
-    
+    private AnimatorStateInfo _animationStateInfo;
+    private int _WPCount = 0;
+    private GameObject _player;
+    private NavMeshAgent _NavMeshAgent;
+
 
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _player = GameObject.Find("FPSController");
+        _NavMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        animationStateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+        _animationStateInfo = _animator.GetCurrentAnimatorStateInfo(0);
         
-        if (animationStateInfo.IsName("Patrol"))
+        //Hearing
+        
+        float distance = Vector3.Distance(transform.position, _player.transform.position);
+        if (distance <= hearingDistance)
         {
-            var target = WayPoints[WPCount];
+            _animator.SetBool("canHearPlayer",true);
+        }
+        else _animator.SetBool("canHearPlayer",false);
+
+        if (_animationStateInfo.IsName("FollowPlayer"))
+        {
+            _NavMeshAgent.SetDestination(_player.transform.position);
+        }
+        
+        //Patrolling
+        
+        if (_animationStateInfo.IsName("Patrol"))
+        {
+            var target = wayPoints[_WPCount];
             if (Vector3.Distance(transform.position, target.transform.position) < 1.0)
             {
                 MoveToNextWP();
             }
 
-            GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
+            _NavMeshAgent.SetDestination(target.transform.position);
         }
+        
+
     }
 
     private void MoveToNextWP()
     {
-        WPCount++;
-        if (WPCount > WayPoints.Count - 1)
+        _WPCount++;
+        if (_WPCount > wayPoints.Count - 1)
         {
-            WPCount = 0;
+            _WPCount = 0;
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position,hearingDistance/2);
     }
 }
